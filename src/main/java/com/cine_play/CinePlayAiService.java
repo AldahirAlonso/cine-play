abdd0c7a0984e1fddf23fb908933e337f26cf086
Aldahir Alonso <aldahir0603@hotmail.com>
2025-08-03 22:32:06 -0600

    Interfaz para LangChain4j

---
+++ src/main/java/com/cine_play/CinePlayAiService.java
@@ -0,0 +1,16 @@
+package com.cine_play;
+
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
+import dev.langchain4j.service.spring.AiService;
+
+@AiService
+public interface CinePlayAiService {
+
+    @UserMessage("""
+            Genera un saludo de bienvenida a la plataforma de Gestion de Peliculas {{plataform}}.
+            Usa menos de 120 caracteres y hazlo de manera formal y educada, y usa emojis por favor.
+            """)
+    String generateGreeting(@V("plataform") String plataform);
+
+}
