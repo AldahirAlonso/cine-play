70b995e738fbd714e2a16f0fb4239e9b6af64bd7
Aldahir Alonso <aldahir0603@hotmail.com>
2025-08-03 22:30:24 -0600

    CinePlayApplication

---
+++ src/main/java/com/cine_play/CinePlayApplication.java
@@ -0,0 +1,13 @@
+package com.cine_play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinePlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinePlayApplication.class, args);
+	}
+
+}
