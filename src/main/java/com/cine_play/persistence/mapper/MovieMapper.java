935d2bd1a51d0d1cdaee8709e72253238aa5a2ad
Aldahir <aldahir0603@hotmail.com>
2025-08-19 20:00:53 -0600

    Convertiendo Entidades a DTO con MapStruct

---
+++ src/main/java/com/cine_play/persistence/mapper/MovieMapper.java
@@ -0,0 +1,19 @@
+package com.cine_play.persistence.mapper;
+
+import com.cine_play.domain.dto.MovieDto;
+import com.cine_play.persistence.entity.MovieEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface MovieMapper {
+    @Mapping(source = "titulo", target = "title")
+    @Mapping(source = "duracion", target = "duration")
+    @Mapping(source = "genero", target = "genre")
+    @Mapping(source = "fechaEstreno", target = "releaseDate")
+    @Mapping(source = "clasificacion", target = "rating")
+    MovieDto toDto(MovieEntity entity);
+    List<MovieDto> toDto(Iterable<MovieEntity> entities);
+}
