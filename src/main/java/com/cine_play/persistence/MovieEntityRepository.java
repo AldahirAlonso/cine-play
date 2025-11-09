6824babedeccb68f74ca693414874c8b8e0607e8
Aldahir <aldahir0603@hotmail.com>
2025-11-09 16:11:31 -0600

    Creacion Endpoint DELETE

--- src/main/java/com/cine_play/persistence/MovieEntityRepository.java
+++ src/main/java/com/cine_play/persistence/MovieEntityRepository.java
@@ -50,4 +50,9 @@ public class MovieEntityRepository implements MovieRepository {
         this.movieMapper.updateEntityFromDto(updateMovieDto, movieEntity);
         return this.movieMapper.toDto(this.crudMovieEntity.save(movieEntity));
     }
+
+    @Override
+    public void delete(long id) {
+        this.crudMovieEntity.deleteById(id);
+    }
 }
