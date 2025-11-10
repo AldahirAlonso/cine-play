c14291c38ea778cfe9ab5ea10edd1d7522180a2f
Aldahir <aldahir0603@hotmail.com>
2025-11-10 17:21:11 -0600

    Funciones para recomendar peliculas por IA

--- src/main/java/com/cine_play/domain/service/CinePlayAiService.java
+++ src/main/java/com/cine_play/domain/service/CinePlayAiService.java
@@ -1,5 +1,6 @@
 package com.cine_play.domain.service;
 
+import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
@@ -12,5 +13,12 @@ public interface CinePlayAiService {
             Usa menos de 120 caracteres y hazlo de manera formal y educada, y usa emojis por favor.
             """)
     String generateGreeting(@V("plataform") String plataform);
+
+    @SystemMessage("""
+            Eres un experto en cine que recomienda peliculas personalizadas segun los gustos de los usuarios. 
+            Debes recomendar maximo 3 peliculas.
+            No incluyas peliculas que esten por fuera de la plataforma de Cine-Play.
+            """)
+    String generateMoviesSuggestion(@UserMessage String userMessage);
 
 }
