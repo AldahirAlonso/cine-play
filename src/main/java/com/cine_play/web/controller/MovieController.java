1bb0a1b41099a8b239f4d0184737f623b7904cc9
Aldahir <aldahir0603@hotmail.com>
2025-11-10 17:21:57 -0600

    Funciones para recomendar peliculas por IA

--- src/main/java/com/cine_play/web/controller/MovieController.java
+++ src/main/java/com/cine_play/web/controller/MovieController.java
@@ -1,8 +1,11 @@
 package com.cine_play.web.controller;
 
 import com.cine_play.domain.dto.MovieDto;
+import com.cine_play.domain.dto.SuggestRequestDto;
 import com.cine_play.domain.dto.UpdateMovieDto;
+import com.cine_play.domain.service.CinePlayAiService;
 import com.cine_play.domain.service.MovieService;
+import dev.langchain4j.service.UserMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +17,11 @@ import java.util.List;
 @RequestMapping("/movies")
 public class MovieController {
     private final MovieService movieService;
+    private final CinePlayAiService aiService;
 
-    public MovieController(MovieService movieService) {
+    public MovieController(MovieService movieService, CinePlayAiService aiService) {
         this.movieService = movieService;
+        this.aiService = aiService;
     }
 
     @GetMapping
@@ -32,6 +37,11 @@ public class MovieController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(movieDto);
+    }
+
+    @PostMapping("/suggest")
+    public ResponseEntity<String> generateMoviesSuggestion(@RequestBody SuggestRequestDto suggestRequestDto) {
+        return ResponseEntity.ok(this.aiService.generateMoviesSuggestion(suggestRequestDto.userPreferences()));
     }
 
     @PostMapping
