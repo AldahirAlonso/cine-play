60596baa88fbac64014646a94016af46c3fa604d
Aldahir <aldahir0603@hotmail.com>
2025-11-10 17:51:18 -0600

    Funcion Excepciones

--- src/main/java/com/cine_play/persistence/crud/CrudMovieEntity.java
+++ src/main/java/com/cine_play/persistence/crud/CrudMovieEntity.java
@@ -4,4 +4,5 @@ import com.cine_play.persistence.entity.MovieEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CrudMovieEntity extends CrudRepository<MovieEntity, Long> {
+    MovieEntity findFirstByTitulo(String titulo);
 }
